be9feeed02414b08958febe79609bf1ae60e00c4
ZiglaCity <ziglacity@gmail.com>
2025-08-28 04:20:38 +0000

    add cors

--- Backend-Starter/src/app.ts
+++ Backend-Starter/src/app.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import birdRoutes from "./routes/bird";
+import cors from "cors";
 
 const app = express();
+// const corsOptions = {
+//   origin: "*",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
 
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/bird", birdRoutes);
